7f0849a5837cce3d16c856cdf3af3dd11da2e945
Abhishek <abhishek.bohra@outlook.com>
2020-08-02 07:46:58 +0530

    added dataloader.py file skeleton

---
+++ utils/dataloader.py
@@ -0,0 +1,5 @@
+def load_jpeg():
+    # This function should read the JPEG folder and apply image augmentations and return the generator object.
+    #Input: None
+    #Output: Data and target in a tuple (data,target)
+    return None
