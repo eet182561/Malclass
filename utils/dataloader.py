16ec7c4d96f2bd9124a3bde8409cd589f14931cc
ankitk50 <ankitk50@gmail.com>
2020-08-02 08:27:16 +0530

    first commit

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -2,4 +2,5 @@ def load_jpeg():
     # This function should read the JPEG folder and apply image augmentations and return the generator object.
     #Input: None
     #Output: Data and target in a tuple (data,target)
+    #just some random comments
     return None
