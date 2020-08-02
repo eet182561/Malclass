728157f26064df3ed032a6c6f9043749d74eb6e7
Abhishek <abhishek.bohra@outlook.com>
2020-08-02 11:21:47 +0530

    Added import statement Keras

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,37 +1,35 @@
-#import tensorflow as tf
+import keras
 
 def load_jpeg():
-    import tensorflow as tf
-
     # This function should read the JPEG folder and apply image augmentations and return the generator object.
     #Input: None
     #Output: Data and target in a tuple (data,target)
     #just some random comments
     directory= "../input/siim-isic-melanoma-classification/jpeg/"
 
-    data_gen=tf.keras.preprocessing.image.ImageDataGenerator(
-    featurewise_center=False,
-    samplewise_center=False,
-    featurewise_std_normalization=False,
-    samplewise_std_normalization=False,
-    zca_whitening=False,
-    zca_epsilon=1e-06,
-    rotation_range=0,
-    width_shift_range=0.0,
-    height_shift_range=0.0,
-    brightness_range=None,
-    shear_range=0.0,
-    zoom_range=0.0,
-    channel_shift_range=0.0,
-    fill_mode="nearest",
-    cval=0.0,
-    horizontal_flip=False,
-    vertical_flip=False,
-    rescale=None,
-    preprocessing_function=None,
-    data_format=None,
-    validation_split=0.0,
-    dtype=None)
+    data_gen = keras.preprocessing.image.ImageDataGenerator(
+        featurewise_center=False,
+        samplewise_center=False,
+        featurewise_std_normalization=False,
+        samplewise_std_normalization=False,
+        zca_whitening=False,
+        zca_epsilon=1e-06,
+        rotation_range=0,
+        width_shift_range=0.0,
+        height_shift_range=0.0,
+        brightness_range=None,
+        shear_range=0.0,
+        zoom_range=0.0,
+        channel_shift_range=0.0,
+        fill_mode="nearest",
+        cval=0.0,
+        horizontal_flip=False,
+        vertical_flip=False,
+        rescale=None,
+        preprocessing_function=None,
+        data_format=None,
+        validation_split=0.0,
+        dtype=None)
 	
 
     train_ds=data_gen.flow_from_directory(
